84f35fe9270df5f40c0f7a47d51db2ce5252a97e
Fatih emin öge <fatiheminoge@gmail.com>
2022-03-17 15:12:46 +0300

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const colors = require("colors");
+const cors = require("cors");
 const postRouter = require("./routes/postRoutes");
 const adminRouter = require("./routes/adminRoutes");
 const connectDb = require("./config/db");
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 connectDb();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
